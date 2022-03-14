00d173196fe77567d4f3c24a12d32e2e141c7545
Takuro Gbemisola <mitchelletakuro@gmail.com>
2022-03-14 11:18:11 +0000

    fixed conflict errors

--- lib/constants/gradient.dart
+++ lib/constants/gradient.dart
@@ -2,12 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:talk2me/constants/colors.dart';
 
 class GradientBg extends StatelessWidget {
-<<<<<<< HEAD
- 
-=======
-  const GradientBg({Key? key}) : super(key: key);
->>>>>>> 70237add6af623d0e7a22920a509c173ce75fb2d
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(

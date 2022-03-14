8c32972b8f4c8ced78482a8b187af2f58115729b
Takuro Gbemisola <mitchelletakuro@gmail.com>
2022-03-14 09:21:43 +0000

    added main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:talk2me/my_app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-    
-    );
-  }
 }

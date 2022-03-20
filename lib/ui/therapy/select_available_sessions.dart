958dc5200729ba51a21244fdec39f2a3f96b9036
Malik Kolade <kolademalikademola@gmail.com>
2022-03-20 14:51:21 +0000

    AppBar corrected to function properly

--- lib/ui/therapy/select_available_sessions.dart
+++ lib/ui/therapy/select_available_sessions.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:step_progress_indicator/step_progress_indicator.dart';
 import 'package:talk2me/constants/colors.dart';
-import 'package:talk2me/widgets/appBar.dart';
+import 'package:talk2me/constants/text_styles.dart' as text_content;
+import 'package:talk2me/widgets/appBar.dart' as app_bar;
 import 'package:talk2me/widgets/range.dart';
 
 class SelectAvailableSessions extends StatefulWidget {
@@ -17,7 +18,7 @@ class _SelectAvailableSessionsState extends State<SelectAvailableSessions> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: AppColors.lightBackground,
-        appBar: AppBarNav(
+        appBar: app_bar.AppBarNavWithBackButton(
           iconColor: AppColors.textColorLightBg,
           onPressed: () {},
         ),
@@ -27,9 +28,30 @@ class _SelectAvailableSessionsState extends State<SelectAvailableSessions> {
               ProgressIndicatorBar(
                 totalSteps: 4,
                 currentStep: 4,
+              ),
+              Container(
+                child: Column(
+                  children: [Wrap()],
+                ),
               )
             ],
           ),
         ));
+  }
+
+  Widget _availableDate(String day, String date, String slot) {
+    return Container(
+      child: Column(
+        children: [
+          text_content.SubtitleTwo(
+              text: date, textColor: AppColors.textColorLightBg),
+          Spacer(),
+          text_content.SubtitleOne(
+              text: date, textColor: AppColors.textColorLightBg),
+          text_content.CaptionText(
+              text: date, textColor: AppColors.subtitleTextLightBg)
+        ],
+      ),
+    );
   }
 }
--- lib/widgets/appBar.dart
+++ lib/widgets/appBar.dart
@@ -5,17 +5,14 @@ import 'package:talk2me/constants/text_styles.dart' as text_content;
 class AppBarNav extends StatelessWidget with PreferredSizeWidget {
   @override
   final Size preferredSize;
-  const AppBarNav(
+  AppBarNav(
       {Key? key,
-      this.onPressed,
-      this.iconColor,
       this.pageHeading = '',
       this.buttonText = '',
       this.buttonColor = Colors.transparent})
       : preferredSize = const Size.fromHeight(40.0),
         super(key: key);
-  final Function? onPressed;
-  final Color? iconColor;
+
   final String pageHeading;
   final String buttonText;
   final Color buttonColor;
@@ -23,14 +20,10 @@ class AppBarNav extends StatelessWidget with PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: _pagetittle(pageHeading, buttonText, buttonColor),
-        leading: IconButton(
-          onPressed: onPressed!(),
-          icon: const Icon(Icons.keyboard_arrow_left),
-          color: iconColor,
-        ));
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      title: _pagetittle(pageHeading, buttonText, buttonColor),
+    );
   }
 
   Widget _pagetittle(String title, String buttonText, Color buttonColor) {
@@ -55,6 +48,42 @@ class AppBarNav extends StatelessWidget with PreferredSizeWidget {
               textColor: AppColors.textColorPrimary,
             ))
       ],
+    );
+  }
+}
+
+class AppBarNavWithBackButton extends StatelessWidget with PreferredSizeWidget {
+  @override
+  final Size preferredSize;
+  AppBarNavWithBackButton({
+    Key? key,
+    required this.onPressed,
+    required this.iconColor,
+    // this.endContent = const Text("Talk2me"),
+    this.pageHeading = '',
+  })  : preferredSize = const Size.fromHeight(40.0),
+        super(key: key);
+  late Function onPressed;
+  final Color iconColor;
+  final String pageHeading;
+  // final Widget? endContent;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        // title: _pagetittle(),
+        leading: IconButton(
+          onPressed: onPressed(),
+          icon: const Icon(Icons.keyboard_arrow_left),
+          color: iconColor,
+        ));
+  }
+
+  Widget _pagetittle(Widget content) {
+    return Row(
+      children: [content],
     );
   }
 }

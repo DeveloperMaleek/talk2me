ce6fd030faa507901edc72e05738e3578f64742f
Malik Kolade <kolademalikademola@gmail.com>
2022-03-20 13:54:25 +0000

    Range and App Bar widget done

--- lib/ui/therapy/select_available_sessions.dart
+++ lib/ui/therapy/select_available_sessions.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
+import 'package:talk2me/constants/colors.dart';
+import 'package:talk2me/widgets/appBar.dart';
+import 'package:talk2me/widgets/range.dart';
 
 class SelectAvailableSessions extends StatefulWidget {
-  const SelectAvailableSessions({ Key? key }) : super(key: key);
+  const SelectAvailableSessions({Key? key}) : super(key: key);
 
   @override
-  State<SelectAvailableSessions> createState() => _SelectAvailableSessionsState();
+  State<SelectAvailableSessions> createState() =>
+      _SelectAvailableSessionsState();
 }
 
 class _SelectAvailableSessionsState extends State<SelectAvailableSessions> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+        backgroundColor: AppColors.lightBackground,
+        appBar: AppBarNav(
+          iconColor: AppColors.textColorLightBg,
+          onPressed: () {},
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              ProgressIndicatorBar(
+                totalSteps: 4,
+                currentStep: 4,
+              )
+            ],
+          ),
+        ));
   }
 }
